e9ad78933de9e26f1939bb5d3c0128134d594f98
Shabda Raaj <shabda@agiliq.com>
2012-07-18 15:47:08 +0530

    Added partials and refactored applications layout. Added css

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,6 +10,11 @@ describe "Static pages" do
       page.should have_content('Sample App')
     end
     
+    it "should have the IE shim" do
+      visit '/static_pages/home'
+      #page.should have_text('http://html5shim.googlecode.com/svn/trunk/html5.js')
+    end
+    
     it "should have right title" do
       visit "/static_pages/home"
         page.should have_selector("title", :text =>"Ruby on Rails Tutorial Sample App")
