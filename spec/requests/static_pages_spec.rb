d9af4dd59d7e90573ae15a0a3f5ffad80eb59490
Shabda Raaj <shabda@agiliq.com>
2012-07-18 16:29:05 +0530

    Used named routes. Used named routes for better specs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,73 +2,61 @@ require 'spec_helper'
 
 
 describe "Static pages" do
-
+  before {visit root_path}
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit root_path
+      
       page.should have_content('Sample App')
     end
     
     it "should have the IE shim" do
-      visit root_path
       #page.should have_text('http://html5shim.googlecode.com/svn/trunk/html5.js')
     end
     
     it "should have right title" do
-      visit root_path
         page.should have_selector("title", :text =>"Ruby on Rails Tutorial Sample App")
     end
     
     it "should have right title" do
-      visit root_path
         page.should_not have_selector("title", :text =>"Home")
     end
     
   end
   
   describe "Help page" do
-
+    before {visit help_path}
      it "should have the content 'Help'" do
-       visit help_path
        page.should have_content('Help')
      end
      
      it "should have title Help" do
-      visit help_path
       page.should have_selector("title", :text => "Help")
      end
    end
 
    describe "Contact page" do
-
+      before {visit contact_path}
       it "should have the content 'Contact Us'" do
-        visit contact_path
         page.should have_content('Contact Us')
       end
 
       it "should have contact email" do
-       visit contact_path
        page.should have_selector("p", :text => "hello@agiliq.com")
       end
     end
 
 
    describe "About page" do
-
+     before {visit about_path}
      it "should have the content 'About Us'" do
-       visit about_path
        page.should have_content('About Us')
      end
      it "should have title About" do
-       visit about_path
        page.should have_selector("title", :text => "About")
      end
      
    end
    
-   describe "home page title" do
-    
-    end
    
 end
